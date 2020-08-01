acb05114a241c4c897e5778a8c92f50c1a6f5525
Constantin Werner <52051213+constantin50@users.noreply.github.com>
2020-08-01 23:28:40 +0700

    Add files via upload

---
+++ morphological_tokenizer.py
@@ -0,0 +1,37 @@
+import mwclient
+import re
+
+class Morpholog:
+  def __init__(self):
+    self.base = mwclient.Site('ru.wiktionary.org')
+
+  def parse(self, word):
+    result = None
+    page = self.base.pages[word]
+    temp = page.text().split("\n\n")
+    for i in range(len(temp)):
+      if ('морфо-ru' in temp[i]):
+        result = temp[i].split('|')[1:]
+    if (result != None):
+      result[-1] = result[-1].replace('}','')
+
+    if 'и=т' in result:
+          del result[result.index('и=т')]
+    return result
+  
+  def get_roots(self, word):
+    result = list()
+    temp = self.parse(word)
+    for i in range(len(temp)):
+      if ('-' not in temp[i] and '+' not in temp[i] and '=' not in temp[i]):
+        result.append(temp[i])
+    return result
+    
+  def define(self, word):
+    page = self.base.pages[word]
+    temp = page.text().split("\n\n")
+    for i in range(len(temp)):
+      if ('=== Значение ===' in temp[i]):
+        result = temp[i].split("\n#")[1:]
+        return result
+    return None
