3bd1684feb4d943907c93edac7b75b106aa561a4
Constantin Werner <52051213+constantin50@users.noreply.github.com>
2020-08-02 13:02:52 +0700

    Update morphological_tokenizer.py

--- morphological_tokenizer.py
+++ morphological_tokenizer.py
@@ -16,19 +16,23 @@ class Morpholog:
       result[-1] = result[-1].replace('}','')
       if 'и=т' in result:
         del result[result.index('и=т')]
+      if len(result[-1]) > 4:
+        del result[-1]
+      if '+' in result[-1]:
+        result[-1] = result[-1][:3]
     return result
   
   def get_roots(self, word):
     result = list()
     temp = self.parse(word)
-    if (temp == None): 
-      return None
-    else:
+    if (temp != None):
       for i in range(len(temp)):
         if ('-' not in temp[i] and '+' not in temp[i] and '=' not in temp[i]):
           result.append(temp[i])
       return result
-    
+    else:
+      return None
+      
   def define(self, word):
     page = self.base.pages[word]
     temp = page.text().split("\n\n")
